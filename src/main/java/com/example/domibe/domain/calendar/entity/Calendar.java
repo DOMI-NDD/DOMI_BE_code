418608a86df3a76a8271f74699e5911a8dcdcf0a
yellowstarr0323 <ihansaem60@gmail.com>
2025-09-23 20:33:52 +0900

    캘린더 엔티티 생성

--- src/main/java/com/example/domibe/domain/calendar/entity/Calendar.java
+++ src/main/java/com/example/domibe/domain/calendar/entity/Calendar.java
@@ -1,4 +1,29 @@
 package com.example.domibe.domain.calendar.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Calendar {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Integer id;
+
+  @Column(nullable = false)
+  private String title;
+
+  @Column(nullable = false)
+  private String detail;
+
+  @Column(nullable = false)
+  private LocalDate date;
 }
--- src/main/java/com/example/domibe/domain/calendar/repository/CalendarRepository.java
+++ src/main/java/com/example/domibe/domain/calendar/repository/CalendarRepository.java
@@ -1,4 +1,7 @@
 package com.example.domibe.domain.calendar.repository;
 
-public interface CalendarRepository {
+import com.example.domibe.domain.calendar.entity.Calendar;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CalendarRepository extends JpaRepository<Calendar, Integer> {
 }
