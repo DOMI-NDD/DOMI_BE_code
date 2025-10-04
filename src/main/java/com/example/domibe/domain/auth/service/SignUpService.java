d6e28c15d1637d84500dd3a1eeae7f630d2945cf
hansaem <ihansaem60@gmail.com>
2025-10-04 20:05:26 +0900

    처음 회원가입 할 때 STUDENT

--- src/main/java/com/example/domibe/domain/auth/service/SignUpService.java
+++ src/main/java/com/example/domibe/domain/auth/service/SignUpService.java
@@ -31,7 +31,7 @@ public class SignUpService {
     User user = User.builder()
         .accountId(signUpRequest.getAccountId())
         .password(passwordEncoder.encode(signUpRequest.getPassword()))
-        .role(Role.ROLE_TEACHER)
+        .role(Role.ROLE_STUDENT)
         .createdAt(LocalDate.now())
         .username(signUpRequest.getUsername())
         .build();
